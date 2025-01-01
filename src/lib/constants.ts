39d42b8f9dfe3505a0c1ff14cc315f0b4704fc29
Dey11 <shreyandey11@gmail.com>
2025-01-01 21:05:22 +0530

    change mail address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const EMAIL = "pdx.use@gmail.com";
+export const EMAIL = "support@usepdx.tech";
 export const INSTAGRAM = "https://www.instagram.com/use.pdx/";
 export const TWITTER = "https://x.com/usepdx_";
 export const TOTAL_GOAL = 50;
