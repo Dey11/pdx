aff2173a693845c6ac32e7a8ebcd71b0b9ea3bfe
Dey11 <shreyandey11@gmail.com>
2025-01-17 03:16:28 +0530

    update navbar ui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,12 +20,12 @@ const headerContents = [
 ];
 const Header = () => {
   return (
-    <header className="sticky top-5 z-50 mx-auto mt-4 flex w-fit rounded-xl bg-[#131415] px-2 py-1 text-[10px] shadow-[0px_0px_3px_#EBE9E9_inset] transition-shadow delay-300 duration-300 ease-in-out hover:shadow-[0px_0px_5px_#00FF1E] sm:text-sm">
-      <ul className="mx-auto flex items-center justify-between py-1 sm:gap-x-16 sm:px-6">
+    <header className="sticky top-5 z-50 mx-auto mt-4 flex w-fit rounded-xl bg-[#131415] px-2 text-[10px] shadow-[0px_0px_3px_#00FF1E_inset] transition-shadow delay-300 duration-300 ease-in-out sm:text-sm">
+      <ul className="mx-auto flex items-center justify-between py-1 sm:gap-x-8 sm:px-6">
         {headerContents.map((item) => {
           return (
             <Link href={item.url} key={item.url} className="">
-              <li className="rounded-lg px-3 font-semibold hover:bg-[#2a2d2f]">
+              <li className="rounded-lg px-3 font-semibold hover:bg-brand-green hover:text-brand-bg hover:underline">
                 {item.name}
               </li>
             </Link>
