34eda9332a6af6b6fbe5c010997f9b8d98a1e1ec
Dey11 <shreyandey11@gmail.com>
2025-01-26 04:00:23 +0530

    fix typo

--- src/app/api/(payments)/plan/route.ts
+++ src/app/api/(payments)/plan/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/db";
 
-export default async function GET() {
+export async function GET() {
   try {
     const session = await auth();
     if (session?.user) {
