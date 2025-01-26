19a557883a0be624bcd0debed5675d1e548724c1
Dey11 <shreyandey11@gmail.com>
2025-01-26 12:50:12 +0530

    del middleware

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/lib/auth";
-
-export default auth((req) => {
-  const publicRoutes = ["/", "/pricing", "/public"];
-  if (!req.auth && !publicRoutes.includes(req.nextUrl.pathname)) {
-    const newUrl = new URL("/login", req.nextUrl.origin);
-    return Response.redirect(newUrl);
-  }
-});
