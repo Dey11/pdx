60a64a6c78a5dd240d6aeb15b17c8f14f90bb7f2
Dey11 <shreyandey11@gmail.com>
2025-01-30 01:07:18 +0530

    integrate posthog and update packages

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,21 @@
-import { SessionProvider } from "next-auth/react";
+"use client";
 
+import { SessionProvider } from "next-auth/react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+  });
+}
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <PostHogProvider client={posthog}>{children}</PostHogProvider>
+    </SessionProvider>
+  );
 };
 
 export default Providers;
