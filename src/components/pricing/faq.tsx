9bcb9820af43d8f62defb86650b487d0719a012d
Dey11 <shreyandey11@gmail.com>
2025-01-29 17:52:17 +0530

    more qna in faq

--- src/components/pricing/faq.tsx
+++ src/components/pricing/faq.tsx
@@ -15,24 +15,39 @@ export function FAQ() {
       question: "What payment methods do you accept?",
       answer: "We accept all major credit cards and UPI.",
     },
-    {
-      question: "Can I switch plans later?",
-      answer: "Yes, you can upgrade or downgrade your plan at any time.",
-    },
+    // {
+    //   question: "Can I switch plans later?",
+    //   answer: "Yes, you can upgrade or downgrade your plan at any time.",
+    // },
     {
       question: "Is there a free trial available?",
       answer:
-        "Yes, we offer a trial plan - it is default and you get it automatically.",
+        "Yes, we offer a trial plan - it is default and you get it automatically. You get 50 credits to start with.",
     },
+    // {
+    //   question: "How do I cancel my subscription?",
+    //   answer: "You can cancel anytime from your account settings.",
+    // },
     {
-      question: "How do I cancel my subscription?",
-      answer: "You can cancel anytime from your account settings.",
-    },
-    {
-      question: "What does unlimited credits mean?",
+      question: "Do you not have any subscription models?",
       answer:
-        "Unlimited credits mean you can use our services as much as you want without any restrictions. Note that we reserve the right to limit usage in case of abuse.",
+        "As of now we don't since we thought a one time model would be more suitable. But we are open to suggestions.",
     },
+    {
+      question: "Are the pricing models inclusive of tax?",
+      answer:
+        "No, the pricing models are exclusive of tax. You will be charged tax as per your region.",
+    },
+    {
+      question: "I paid for credits but I don't see them in my account?",
+      answer:
+        "We update the transactions as soon as we receive confirmation from the payment gateway. If you have an email from Dodo Payments confirming your purchase and yet do not see the benefits in your account in 30 minutes to 1 hour, please contact us at support@usepdx.tech. We will make it our priority to help you asap.",
+    },
+    // {
+    //   question: "What does unlimited credits mean?",
+    //   answer:
+    //     "Unlimited credits mean you can use our services as much as you want without any restrictions. Note that we reserve the right to limit usage in case of abuse.",
+    // },
   ];
 
   return (
