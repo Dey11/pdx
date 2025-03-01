12a498abe62b12c82a200fe78af3a635c0ae7518
Dey11 <shreyandey11@gmail.com>
2025-03-02 00:28:14 +0530

    ui fix

--- src/components/home/features-section.tsx
+++ src/components/home/features-section.tsx
@@ -56,7 +56,7 @@ const FeaturesSection = () => {
               <div className="flex" key={feature.id}>
                 <div
                   className={cn(
-                    `m-2 size-3 flex-shrink-0 rounded-full`,
+                    `m-2.5 size-3 flex-shrink-0 rounded-full`,
                     feature.id === currFeat ? "bg-brand-blue" : "bg-[#424242]"
                   )}
                 />
